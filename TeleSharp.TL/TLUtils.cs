d4b9c88866d66808615c1064bd322afc5874e8c5
Viktor Borisov <vik.borisov@outlook.com>
2017-04-13 13:48:29 +0700

    remove unused dependency BigMath

--- TeleSharp.TL/TLUtils.cs
+++ TeleSharp.TL/TLUtils.cs
@@ -1,7 +1,5 @@
 ﻿using System.IO;
 using System.Text;
-using BigMath;
-using BigMath.Utils;
 
 namespace TeleSharp.TL
 {
@@ -158,32 +156,6 @@ namespace TeleSharp.TL
         public static void Serialize(long src, BinaryWriter writer)
         {
             writer.Write(src);
-        }
-    }
-
-    public class Int128Util
-    {
-        public static Int128 Deserialize(BinaryReader reader)
-        {
-            return reader.ReadBytes(16).ToInt128(0, true);
-        }
-
-        public static void Serialize(Int128 src, BinaryWriter writer)
-        {
-            writer.Write(src.ToBytes(true));
-        }
-    }
-
-    public class Int256Util
-    {
-        public static Int256 Deserialize(BinaryReader reader)
-        {
-            return reader.ReadBytes(32).ToInt256(0, true);
-        }
-
-        public static void Serialize(Int256 src, BinaryWriter writer)
-        {
-            writer.Write(src.ToBytes(true));
         }
     }
 }
