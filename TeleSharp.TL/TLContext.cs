c443ad0cc0d6d7b72a787fd0124f33fbd1b388f0
LORDofDOOM <tb@bbz-int.de>
2018-03-22 04:01:20 +0100

    [Core] Changed type check as suggested by @knocte

--- TeleSharp.TL/TLContext.cs
+++ TeleSharp.TL/TLContext.cs
@@ -20,11 +20,7 @@ namespace TeleSharp.TL
                         where t.IsSubclassOf(typeof(TLObject))
                         where t.GetCustomAttribute(typeof(TLObjectAttribute)) != null
                         select t).ToDictionary(x => ((TLObjectAttribute)x.GetCustomAttribute(typeof(TLObjectAttribute))).Constructor, x => x);
-
-            if (!Types.ContainsKey(481674261))
-            {
-                Types.Add(481674261, typeof(TLVector<>));
-            }
+            Types[481674261] = typeof(TLVector<>);
         }
         public static Type getType(int Constructor)
         {
