sochix/TLSharp

commit 3224113e4ce827319c049f44ee5a759ad1591fa0

Modified TcpClient setup to work with IPv6 addresses, fixing #773.

TLSharp.Core/Network/TcpTransport.cs
@@ -16,10 +16,11 @@ namespace TLSharp.Core.Network
         {
             if (handler == null)
             {
-                _tcpClient = new TcpClient();
-
                 var ipAddress = IPAddress.Parse(address);
-                _tcpClient.Connect(ipAddress, port);
+                var endpoint = new IPEndPoint(ipAddress, port);
+
+                _tcpClient = new TcpClient(endpoint);
+                _tcpClient.Connect(endpoint);
             }
             else
                 _tcpClient = handler(address, port);

commit 00330c9bd010945285e56027345d3f37bcf4792a

Fix Issue #862 (#863)
Fix connection with IPv4 address. A regression introduced in 3224113e4ce827319c049f44ee5a759ad1591fa0.

TLSharp.Core/Network/TcpTransport.cs
@@ -19,7 +19,7 @@ namespace TLSharp.Core.Network
                 var ipAddress = IPAddress.Parse(address);
                 var endpoint = new IPEndPoint(ipAddress, port);
 
-                _tcpClient = new TcpClient(endpoint);
+                _tcpClient = new TcpClient(ipAddress.AddressFamily);
                 _tcpClient.Connect(endpoint);
             }
             else