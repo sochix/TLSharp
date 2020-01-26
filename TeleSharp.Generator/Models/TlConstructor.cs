d42a367cf4d0574153e9186fa7fbfa3f08112a3f
CheshireCaat <likangt2012@gmail.com>
2020-01-26 05:03:28 +0300

    TeleSharp.Generator refactoring
    Fix naming for models properties (Use JsonProperty instead)
    Move models to separate folder
    Rename models
    
    Remove unused references in Program.cs
    Use string instead of String

---
+++ TeleSharp.Generator/Models/TlConstructor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TeleSharp.Generator.Models
+{
+    internal class TlConstructor
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("predicate")] 
+        public string Predicate { get; set; }
+
+        [JsonProperty("params")] 
+        public List<TlParam> Params { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+}
---
+++ TeleSharp.Generator/Models/TlSchema.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace TeleSharp.Generator.Models
+{
+    internal class TlSchema
+    {
+        [JsonProperty("constructors")] 
+        public List<TlConstructor> Constructors { get; set; }
+
+        [JsonProperty("methods")] 
+        public List<TlMethod> Methods { get; set; }
+    }
+}
