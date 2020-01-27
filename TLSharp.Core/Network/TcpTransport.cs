c00b48ede098f2747d15844d0fe73fd2a9b5de20
CheshireCaat <likangt2012@gmail.com>
2020-01-27 10:03:45 +0300

    Improvements for async methods
    Added CancellationToken with default value to all async methods in TLSharp.Core
    Added ConfigureAwait(false) to all async methods in TLSharp.Core
    SendRequestAsync replaced to SendAuthenticatedRequestAsync in methods that need auth
    Private modifier in SendAuthenticatedRequestAsync changed to internal
    
    Based on PR created by @IaRuslan

--- TLSharp.Core/Network/TcpTransport.cs
+++ TLSharp.Core/Network/TcpTransport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TLSharp.Core.Network
@@ -32,26 +33,26 @@ namespace TLSharp.Core.Network
             }
         }
 
-        public async Task Send(byte[] packet)
+        public async Task Send(byte[] packet, CancellationToken token = default(CancellationToken))
         {
             if (!_tcpClient.Connected)
                 throw new InvalidOperationException("Client not connected to server.");
 
             var tcpMessage = new TcpMessage(sendCounter, packet);
 
-            await _stream.WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length);
+            await _stream.WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length, token).ConfigureAwait(false);
             sendCounter++;
         }
 
-        public async Task<TcpMessage> Receive()
+        public async Task<TcpMessage> Receive(CancellationToken token = default(CancellationToken))
         {
             var packetLengthBytes = new byte[4];
-            if (await _stream.ReadAsync(packetLengthBytes, 0, 4) != 4)
+            if (await _stream.ReadAsync(packetLengthBytes, 0, 4, token).ConfigureAwait(false) != 4)
                 throw new InvalidOperationException("Couldn't read the packet length");
             int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
 
             var seqBytes = new byte[4];
-            if (await _stream.ReadAsync(seqBytes, 0, 4) != 4)
+            if (await _stream.ReadAsync(seqBytes, 0, 4, token).ConfigureAwait(false) != 4)
                 throw new InvalidOperationException("Couldn't read the sequence");
             int seq = BitConverter.ToInt32(seqBytes, 0);
 
@@ -62,7 +63,7 @@ namespace TLSharp.Core.Network
             do
             {
                 var bodyByte = new byte[packetLength - 12];
-                var availableBytes = await _stream.ReadAsync(bodyByte, 0, neededToRead);
+                var availableBytes = await _stream.ReadAsync(bodyByte, 0, neededToRead, token).ConfigureAwait(false);
                 neededToRead -= availableBytes;
                 Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
                 readBytes += availableBytes;
@@ -70,7 +71,7 @@ namespace TLSharp.Core.Network
             while (readBytes != packetLength - 12);
 
             var crcBytes = new byte[4];
-            if (await _stream.ReadAsync(crcBytes, 0, 4) != 4)
+            if (await _stream.ReadAsync(crcBytes, 0, 4, token).ConfigureAwait(false) != 4)
                 throw new InvalidOperationException("Couldn't read the crc");
             int checksum = BitConverter.ToInt32(crcBytes, 0);
 
