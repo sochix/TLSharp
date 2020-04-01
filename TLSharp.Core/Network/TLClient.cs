2d9aa26ba22702e64b356e98a1600c189482ce27
Afshin Arani <afshinarani79@gmail.com>
2020-04-01 12:56:22 +0430

    Update to .net 4.6 and async try

---
+++ TLSharp.Core/Network/TLClient.cs
@@ -0,0 +1,57 @@
+﻿using NetCoreServer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLSharp.Core.Network
+{
+    class TLClient : TcpClient
+    {
+        public delegate void ReceiveQueue(TcpMessage message);
+        private readonly ReceiveQueue receiveQueue;
+        public TLClient(IPAddress address, int port, ReceiveQueue queue) : base(address, port)
+        {
+            receiveQueue = queue;
+        }
+
+        protected override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            var packetLengthBytes = new byte[4];
+            packetLengthBytes = buffer.Take(4).ToArray();
+            int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
+
+            var seqBytes = new byte[4];
+            seqBytes = buffer.Skip(4).Take(4).ToArray();
+            int seq = BitConverter.ToInt32(seqBytes, 0);
+
+            int readBytes = 0;
+            var body = new byte[packetLength - 12];
+            body = buffer.Skip(8).Take(packetLength - 12).ToArray();
+
+            var crcBytes = new byte[4];
+            crcBytes = buffer.Skip(packetLength - 4).Take(4).ToArray();
+            int checksum = BitConverter.ToInt32(crcBytes, 0);
+
+            byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
+
+            System.Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
+            System.Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
+            System.Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
+            var crc32 = new Ionic.Crc.CRC32();
+            crc32.SlurpBlock(rv, 0, rv.Length);
+            var validChecksum = crc32.Crc32Result;
+
+            if (checksum != validChecksum)
+            {
+                throw new InvalidOperationException("invalid checksum! skip");
+            }
+
+            receiveQueue(new TcpMessage(seq, body));
+        }
+
+
+    }
+}
--- TLSharp.Core/Network/TcpTransport.cs
+++ TLSharp.Core/Network/TcpTransport.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Threading;
@@ -10,103 +12,64 @@ namespace TLSharp.Core.Network
 
     public class TcpTransport : IDisposable
     {
-        private readonly TcpClient tcpClient;
-        private readonly NetworkStream stream;
+        private readonly TLClient tcpClient;
+        private readonly BlockingCollection<TcpMessage> ReceievedMessage = new BlockingCollection<TcpMessage>();
+        //private readonly NetworkStream stream;
         private int sendCounter = 0;
 
         public TcpTransport(string address, int port, TcpClientConnectionHandler handler = null)
         {
-            if (handler == null)
-            {
-                var ipAddress = IPAddress.Parse(address);
-                var endpoint = new IPEndPoint(ipAddress, port);
+            //if (handler == null)
+            //{
+            var ipAddress = IPAddress.Parse(address);
+            tcpClient = new TLClient(ipAddress, port, ReceiveMessage);
+            tcpClient.ConnectAsync();
+            //}
+            //else
+            //    tcpClient = handler(address, port);
+        }
 
-                tcpClient = new TcpClient(ipAddress.AddressFamily);
-                tcpClient.Connect(endpoint);
-            }
-            else
-                tcpClient = handler(address, port);
+        public void ReceiveMessage(TcpMessage message)
+        {
+            ReceievedMessage.Add(message);
+            //handle
 
-            if (tcpClient.Connected)
-            {
-                stream = tcpClient.GetStream();
-            }
         }
 
         public async Task Send(byte[] packet, CancellationToken token = default(CancellationToken))
         {
-            if (!tcpClient.Connected)
+            if (!tcpClient.IsConnected)
                 throw new InvalidOperationException("Client not connected to server.");
 
             var tcpMessage = new TcpMessage(sendCounter, packet);
 
-            await stream.WriteAsync(tcpMessage.Encode(), 0, tcpMessage.Encode().Length, token).ConfigureAwait(false);
+            tcpClient.SendAsync(tcpMessage.Encode());
             sendCounter++;
         }
 
         public async Task<TcpMessage> Receive(CancellationToken token = default(CancellationToken))
         {
-            var packetLengthBytes = new byte[4];
-            if (await stream.ReadAsync(packetLengthBytes, 0, 4, token).ConfigureAwait(false) != 4)
-                throw new InvalidOperationException("Couldn't read the packet length");
-            int packetLength = BitConverter.ToInt32(packetLengthBytes, 0);
-
-            var seqBytes = new byte[4];
-            if (await stream.ReadAsync(seqBytes, 0, 4, token).ConfigureAwait(false) != 4)
-                throw new InvalidOperationException("Couldn't read the sequence");
-            int seq = BitConverter.ToInt32(seqBytes, 0);
-
-            int readBytes = 0;
-            var body = new byte[packetLength - 12];
-            int neededToRead = packetLength - 12;
-
-            do
-            {
-                var bodyByte = new byte[packetLength - 12];
-                var availableBytes = await stream.ReadAsync(bodyByte, 0, neededToRead, token).ConfigureAwait(false);
-                neededToRead -= availableBytes;
-                Buffer.BlockCopy(bodyByte, 0, body, readBytes, availableBytes);
-                readBytes += availableBytes;
-            }
-            while (readBytes != packetLength - 12);
-
-            var crcBytes = new byte[4];
-            if (await stream.ReadAsync(crcBytes, 0, 4, token).ConfigureAwait(false) != 4)
-                throw new InvalidOperationException("Couldn't read the crc");
-            int checksum = BitConverter.ToInt32(crcBytes, 0);
-
-            byte[] rv = new byte[packetLengthBytes.Length + seqBytes.Length + body.Length];
-
-            Buffer.BlockCopy(packetLengthBytes, 0, rv, 0, packetLengthBytes.Length);
-            Buffer.BlockCopy(seqBytes, 0, rv, packetLengthBytes.Length, seqBytes.Length);
-            Buffer.BlockCopy(body, 0, rv, packetLengthBytes.Length + seqBytes.Length, body.Length);
-            var crc32 = new Ionic.Crc.CRC32();
-            crc32.SlurpBlock(rv, 0, rv.Length);
-            var validChecksum = crc32.Crc32Result;
-
-            if (checksum != validChecksum)
-            {
-                throw new InvalidOperationException("invalid checksum! skip");
-            }
-
-            return new TcpMessage(seq, body);
+            return ReceievedMessage.Take();
         }
+
+
 
         public bool IsConnected
         {
             get
             {
-                return this.tcpClient.Connected;
+                ; return this.tcpClient.IsConnected;
             }
         }
 
 
         public void Dispose()
         {
-            if (tcpClient.Connected)
+            if (tcpClient.IsConnected)
             {
-                stream.Close();
-                tcpClient.Close();
+                //stream.Close();
+                tcpClient.DisconnectAsync();
+                tcpClient.Dispose();
             }
         }
     }
