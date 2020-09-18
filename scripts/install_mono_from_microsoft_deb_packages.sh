652c7d6cbbe0b107fdcee38f8a7ddd856f04b671
Andres G. Aragoneses <knocte@gmail.com>
2020-09-18 14:50:03 +0800

    GitLabCI: remove old mono version pipeline
    Because it is red (because nuget doesn't accept old TLS connections,
    and only newer versions of Mono support newer TLS versions)

--- scripts/install_mono_from_microsoft_deb_packages.sh
+++ scripts/install_mono_from_microsoft_deb_packages.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+source /etc/os-release
+
 # required by apt-key
 apt install -y gnupg2
 # required by apt-update when pulling from mono-project.com
@@ -8,8 +10,8 @@ apt install -y ca-certificates
 
 # taken from http://www.mono-project.com/download/stable/#download-lin
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-# bionic(=18.04) below works even for 18.10, 19.04 and 19.10
-echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+echo "deb https://download.mono-project.com/repo/ubuntu stable-$UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/mono-official-stable.list
+
 apt update
-apt install -y mono-devel
+DEBIAN_FRONTEND=noninteractive apt install -y mono-devel
 mono --version
