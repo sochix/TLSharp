90aef81908b8bd5f9a3cf1f79861a7915f4f5e28
Andres G. Aragoneses <knocte@gmail.com>
2020-10-17 22:55:32 +0200

    Core: improve flood exception msg

--- src/TgSharp.Core/Network/Exceptions/FloodException.cs
+++ src/TgSharp.Core/Network/Exceptions/FloodException.cs
@@ -7,7 +7,7 @@ namespace TgSharp.Core.Network.Exceptions
         public TimeSpan TimeToWait { get; private set; }
 
         internal FloodException(TimeSpan timeToWait)
-            : base($"Flood prevention. Telegram now requires your program to do requests again only after {timeToWait.TotalSeconds} seconds have passed ({nameof(TimeToWait)} property)." +
+            : base($"Flood prevention. Telegram now requires your program to do requests again only after {timeToWait.TotalSeconds} seconds have passed ({nameof(TimeToWait)} property), so if now it's {DateTime.Now}, wait until {(DateTime.Now+timeToWait)}" +
                    " If you think the culprit of this problem may lie in TgSharp's implementation, open a Github issue please.")
         {
             TimeToWait = timeToWait;
